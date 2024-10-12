0cf15b3bebe78d43bacd254190d9c01cc733b15a
Rodrigo Zaroni <rodrigo@zaroni.com.br>
2024-10-11 21:44:48 -0300

    eslint-disable @typescript-eslint/no-unused-vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import React, { useState, useEffect } from 'react';
 import { Search, Calendar, MapPin, LogOut, QrCode, Filter } from 'lucide-react';
